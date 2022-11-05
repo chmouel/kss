4a869c54ac6669befe241abf2eb441acba25b407
Chmouel Boudjnah <chmouel@chmouel.com>
2022-11-05 22:53:15 +0100

    fix token
    Signed-off-by: Chmouel Boudjnah <chmouel@chmouel.com>

--- hack/update-homebrew-formula.sh
+++ hack/update-homebrew-formula.sh
@@ -6,14 +6,14 @@ clean() { rm -f ${TMP}; }
 trap clean EXIT
 set -x
 
-curl -f -H "Authorization: $(pass show chmouel/github-token)" https://api.github.com/repos/chmouel/kss/releases  > ${TMP}
+curl -f -H "Authorization: $(pass show github/chmouel-token)" https://api.github.com/repos/chmouel/kss/releases  > ${TMP}
 
 tarball=$(jq -r '.[0].tarball_url' ${TMP})
 [[ -z ${tarball} ]] && { echo "no tarball found??"; exit 1;}
 
 version=$(jq -r '.[0].tag_name' ${TMP})
 [[ -z ${version} ]] && { echo "no version found??"; exit 1;}
-curl -H"Authorization: $(pass show chmouel/github-token)"  -# -o ${TMP} ${tarball}
+curl -H"Authorization: $(pass show github/chmouel-token)"  -# -o ${TMP} ${tarball}
 
 shasum=$(sha256sum ${TMP}|cut -d" " -f1)
 sed -i "s/sha256 \".*\"/sha256 \"${shasum}\"/;s/version \".*\"/version \"${version}\"/" ../Formula/kss.rb
